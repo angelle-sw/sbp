b781f2a64cc9c6406319562039ca3fab54964794
JP Angelle <jp@angelle.me>
2020-12-14 14:03:32 -0600

    add dynamic import

--- client/src/sbp.ts
+++ client/src/sbp.ts
@@ -2,13 +2,17 @@ import { Contract } from '@ethersproject/contracts';
 import { abi, address } from './contract-data.json';
 import web3 from './web3';
 
-const { NODE_ENV, REACT_APP_CONTRACT_ADDRESS } = process.env;
+const { NODE_ENV } = process.env;
 
-const getAddress = () => {
+const getAddress = async () => {
   if (NODE_ENV === 'production') {
     return address;
   }
-  return REACT_APP_CONTRACT_ADDRESS;
+
+  // @ts-ignore
+  const contract = await import('./contract-data-local.json');
+  // @ts-ignore
+  return contract.address;
 };
 
 const contractAddress = getAddress();
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,27 +2,22 @@ const fs = require('fs');
 const path = require('path');
 const Sbp = artifacts.require('../contracts/Sbp.sol');
 
-const updateEnvFile = address => {
-  const envs = fs.readFileSync(path.join(__dirname, '../client/.env'), 'utf8');
+const updateContractDataLocalData = address => {
+  const contractDataLocalPath = path.join(
+    __dirname,
+    '../client/src/contract-data-local.json',
+  );
 
-  const array = envs.split('\n');
-
-  const envObj = array.reduce((acc, cur) => {
-    const [key, value] = cur.split('=');
-    acc[key] = value;
-    return acc;
-  }, {});
-
-  envObj.REACT_APP_CONTRACT_ADDRESS = address;
-
-  const newEnvs = Object.entries(envObj).map(env => {
-    const [key, value] = env;
-    return `${key}=${value}`;
-  });
-
-  const joined = newEnvs.join('\n');
-
-  fs.writeFileSync(path.join(__dirname, '../client/.env'), joined);
+  fs.writeFileSync(
+    contractDataLocalPath,
+    JSON.stringify(
+      {
+        address,
+      },
+      null,
+      2,
+    ),
+  );
 };
 
 const updateContractData = ({ abi, networks }) => {
@@ -41,7 +36,7 @@ const updateContractData = ({ abi, networks }) => {
   // only update the env file if a local contract address exist
   if (networks[5777] && networks[5777].address) {
     const address = networks[5777].address;
-    updateEnvFile(address);
+    updateContractDataLocalData(address);
   }
 };
 
