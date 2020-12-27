75e63eef5c0a57df590e20fe1d30f5dd6cb632b7
Zak Angelle <zaknotzak@gmail.com>
2020-12-27 01:24:35 -0600

    Refactor test helpers

--- test/helpers.js
+++ test/helpers.js
@@ -1,3 +1,5 @@
+const { constants } = require('@openzeppelin/test-helpers');
+
 const EMPTY_EVENT = {
   option1: '',
   option2: '',
@@ -6,16 +8,16 @@ const EMPTY_EVENT = {
 };
 
 const EMPTY_BET = {
-  bettor: '0x0000000000000000000000000000000000000000',
+  bettor: constants.ZERO_ADDRESS,
   eventId: '0',
   option: '0',
   amount: '0',
   claimed: '0'
 };
 
-const isEmptyEvent = (event) => {
-  for (const prop in EMPTY_EVENT) {
-    if (event[prop] !== EMPTY_EVENT[prop]) {
+const isEmptyObject = (obj, emptyObjFixture) => {
+  for (const prop in emptyObjFixture) {
+    if (obj[prop] !== emptyObjFixture[prop]) {
       return false;
     }
   }
@@ -23,14 +25,7 @@ const isEmptyEvent = (event) => {
   return true;
 };
 
-const isEmptyBet = (bet) => {
-  for (const prop in EMPTY_BET) {
-    if (bet[prop] !== EMPTY_BET[prop]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const isEmptyEvent = (event) => isEmptyObject(event, EMPTY_EVENT);
+const isEmptyBet = (bet) => isEmptyObject(bet, EMPTY_BET);
 
 module.exports = { isEmptyEvent, isEmptyBet };
